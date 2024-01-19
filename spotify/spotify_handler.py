b3c19003d7d4830ec3e90070b7a9593630283e1e
dominik <dominik.pasymowski@gmail.com>
2024-01-19 15:03:19 +0100

    handle spotify API for gestures

--- spotify/spotify_handler.py
+++ spotify/spotify_handler.py
@@ -13,7 +13,7 @@ def init_spotify():
     client_id = validate_and_return_env_variable('SPOTIFY_CLIENT_ID')
     client_secret = validate_and_return_env_variable('SPOTIFY_SECRET')
 
-    scope = "user-modify-playback-state"
+    scope = "user-modify-playback-state,user-read-playback-state"
 
     return spotipy.Spotify(
         auth_manager=SpotifyOAuth(
@@ -37,6 +37,7 @@ class SpotifyHandler:
     def __init__(self):
         self.last_known = ""
         self.spotify = init_spotify()
+        self.last_known_volume_percent = None
 
     def handle_hand_gesture(self, hand_gesture):
         if self.last_known != hand_gesture:
@@ -46,5 +47,17 @@ class SpotifyHandler:
                 self.spotify.pause_playback()
             if hand_gesture == 'Resume':
                 self.spotify.start_playback()
+            if hand_gesture == 'Mute':
+                device_volume_percent = self.spotify.current_playback()['device']['volume_percent']
+                if device_volume_percent != 0:
+                    self.last_known_volume_percent = self.spotify.current_playback()['device']['volume_percent']
+                print("Setting volume to 0, previous known volume: " + str(self.last_known_volume_percent))
+                self.spotify.volume(0)
+            if hand_gesture == 'Unmute' and self.last_known_volume_percent is not None:
+                self.spotify.volume(self.last_known_volume_percent)
+            if hand_gesture == 'Next song':
+                self.spotify.next_track()
+            if hand_gesture == 'Previous song':
+                self.spotify.previous_track()
 
         self.last_known = hand_gesture
